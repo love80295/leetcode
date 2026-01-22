455bffa7fdf92724a2b486c3f95123289b362adf
love80295 <167100948+love80295@users.noreply.github.com>
2026-01-22 22:43:09 +0530

    Time: 1 ms (99.64%), Space: 46.5 MB (96.69%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+       ArrayList<String> st = new ArrayList<>();
+       for(int i = 1 ; i<= n ; i++){
+        if(i%3==0 && i%5==0){
+            st.add("FizzBuzz");
+        }else if(i%3==0){
+            st.add("Fizz");
+        }else if(i%5==0){
+            st.add("Buzz");
+        }else{
+            String str = String.valueOf(i);
+            st.add(str);
+        }
+       }
+       return st; 
+    }
+}
