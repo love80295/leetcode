6ad331e57025f3e9783af266cea64aad46857897
love80295 <167100948+love80295@users.noreply.github.com>
2026-03-10 04:29:05 +0530

    Time: 19 ms (11.36%), Space: 46.5 MB (32.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,43 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
               if(s.length()!=t.length()) return false;
-        HashMap<Character , Integer> mp1 = maps(s);
-        HashMap<Character , Integer> mp2 = maps(t);
-        return mp1.equals(mp2);
-    }
-      static HashMap<Character , Integer> maps(String s){
-        HashMap<Character , Integer> mp = new HashMap<>();
+        // HashMap<Character , Integer> mp1 = maps(s);
+        // HashMap<Character , Integer> mp2 = maps(t);
+        // return mp1.equals(mp2);
+        HashMap<Character , Integer> map= new HashMap<>();
         for(int i = 0 ; i<s.length() ; i++){
             char ch = s.charAt(i);
-            if(mp.containsKey(ch)){
-                int freq = mp.get(ch);
-                mp.put(ch , freq+1);
+            if(!map.containsKey(ch)){
+                map.put(ch , 1);
             }else{
-                mp.put(ch , 1);
+                map.put(ch , map.get(ch)+1);
             }
-
         }
-        return mp;
+        for(int i = 0 ; i<t.length() ; i++ ){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch , map.get(ch)-1);
+            } else{
+                return false;
+            }
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
+        }
+        if(map.size()==0) return true;
+        else return false;
     }
-}
+    //   static HashMap<Character , Integer> maps(String s){
+    //     HashMap<Character , Integer> mp = new HashMap<>();
+    //     for(int i = 0 ; i<s.length() ; i++){
+    //         char ch = s.charAt(i);
+    //         if(mp.containsKey(ch)){
+    //             int freq = mp.get(ch);
+    //             mp.put(ch , freq+1);
+    //         }else{
+    //             mp.put(ch , 1);
+    //         }
+
+    //     }
+    //     return mp;
+    }
