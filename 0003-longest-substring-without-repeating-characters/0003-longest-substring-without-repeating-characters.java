6375dca2337609be74851c289f70a0728ddbbc51
love80295 <167100948+love80295@users.noreply.github.com>
2026-03-10 06:00:56 +0530

    Time: 5 ms (86.56%), Space: 45.7 MB (72.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,40 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // HashMap<Character , Integer> map = new HashMap<>();
+        // int left = 0;
+        // // int right = 0;
+        // int maxlen = 0;
+        // for(int i = 0 ; i<s.length() ; i++){
+        //     char ch = s.charAt(i);
+        //     if(!map.containsKey(ch)){
+        //         map.put(ch , i);
+        //     }else{
+        //         int val = map.get(ch);
+        //         // if(left>val) continue;
+        //         // left = val+1;
+        //         left = Math.max(left , val+1);
+        //         map.put(ch , i);
+        //     }
+        //      int len = (i-left)+1;
+        //      maxlen = Math.max(len , maxlen);
+
+        // }
+        // return maxlen;
         HashMap<Character , Integer> map = new HashMap<>();
         int left = 0;
-        // int right = 0;
         int maxlen = 0;
-        for(int i = 0 ; i<s.length() ; i++){
-            char ch = s.charAt(i);
+        for(int right = 0; right<s.length() ; right++){
+            char ch = s.charAt(right);
             if(!map.containsKey(ch)){
-                map.put(ch , i);
+                map.put(ch , right);
             }else{
                 int val = map.get(ch);
-                // if(left>val) continue;
-                // left = val+1;
                 left = Math.max(left , val+1);
-                map.put(ch , i);
+                map.put(ch , right);
             }
-             int len = (i-left)+1;
-             maxlen = Math.max(len , maxlen);
-
+            int len = (right-left)+1;
+            maxlen = Math.max(len , maxlen);
         }
         return maxlen;
-
     }
 }
