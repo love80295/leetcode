6b7247761cbf6fe800da005ae3d38de64d4e2d49
love80295 <167100948+love80295@users.noreply.github.com>
2026-03-30 21:30:49 +0530

    Time: 1 ms (99.92%), Space: 46.7 MB (31.06%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-     for(int i = 0 ; i<9 ; i++){
-        for(int j = 0 ; j<9 ; j++){
-            if(board[i][j]=='.') continue;
-             char num = board[i][j];
-            board[i][j] = '.';
-            if(isSafe(board , i , j  , num)==false) return false;
-            board[i][j] = num;
+        for(int i = 0 ; i<9 ; i++){
+            for(int j = 0 ; j<9 ; j++){
+                if(board[i][j]=='.') continue;
+                char ch = board[i][j];
+                board[i][j]='.';
+                if(isValid(board , i , j , ch)==false) return false;
+                board[i][j] = ch;
+            }
         }
-        
-     }
-     return true;  
+        return true;
     }
-    public boolean isSafe(char[][]board , int row , int col , char num){
-        // check row
+    static boolean isValid(char[][]board , int row , int col , char num){
+        // chack row
         for(int j = 0 ; j<9 ; j++){
             if(board[row][j]==num) return false;
         }
-        // check col
+        // checkk column
         for(int i = 0 ; i<9 ; i++){
             if(board[i][col]==num) return false;
         }
-        // check matrix
+        // checkk grid
         int sr = row/3*3;
         int sc = col/3*3;
         for(int i = sr ; i<sr+3 ; i++){
