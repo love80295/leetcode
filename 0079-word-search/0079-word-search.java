abcf62977c48ba6e6b0501e3971e71ff77912735
love80295 <167100948+love80295@users.noreply.github.com>
2026-03-31 07:19:46 +0530

    Time: 127 ms (88.89%), Space: 43.1 MB (37.26%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-       for(int i = 0 ; i<board.length ; i++){
-        for(int j = 0 ; j<board[0].length ; j++){
-            if(board[i][j]==word.charAt(0)){
-                if(isValid(board , i , j , 0 , word )) return true;
+        for(int i = 0 ; i<board.length ; i++){
+            for(int j  = 0 ; j<board[0].length ;j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(isMatch(board , i , j , word , 0)==true) return true; 
+                }
             }
         }
-       }
-       return false;
+        return false;
     }
-    public boolean isValid(char[][] board , int row ,  int col , int idx , String word){
-        if(idx==word.length()){
+    static boolean isMatch(char[][]board , int row , int col , String s , int idx){
+        if(idx==s.length()){
             return true;
         }
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(idx)) return false; 
-           
-       char ch = board[row][col];
-       board[row][col] = '#';
-       boolean ans = isValid(board , row , col+1 , idx+1 , word ) ||
-                    isValid(board , row , col-1 , idx+1 , word ) ||
-                    isValid(board , row+1 , col , idx+1 , word ) ||
-                    isValid(board , row-1 , col , idx+1 , word ); 
-    board[row][col] = ch;
-    return ans;
+       if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != s.charAt(idx)) return false; 
+        char ch = board[row][col];
+        board[row][col] = '#';
+        boolean ans = isMatch(board , row , col+1 , s , idx+1) ||
+                      isMatch(board , row , col-1 , s , idx+1) ||
+                      isMatch(board , row-1 , col , s , idx+1) ||
+                      isMatch(board , row+1 , col , s , idx+1); 
+     board[row][col] = ch;
+    return ans;                  
     }
 }
