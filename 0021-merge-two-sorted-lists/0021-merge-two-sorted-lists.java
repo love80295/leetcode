75d93709c671b007eb7379f08220f4a765ce8b6d
love80295 <167100948+love80295@users.noreply.github.com>
2026-04-07 15:14:00 +0530

    Time: 1 ms (3.22%), Space: 44.6 MB (12.2%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,51 +10,33 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // ListNode temp1 = list1;
-        // ListNode temp2 = list2;
-        // ListNode head = new ListNode(1010);
-        // ListNode temp  = head;
-        // while(temp1!=null && temp2!=null){
-        //     if(temp1.val<temp2.val){
-        //         ListNode a = new ListNode(temp1.val);
-        //         temp.next = a;
-        //         temp = a;
-        //         temp1 = temp1.next;
-        //     } else{
-        //         ListNode a = new ListNode(temp2.val);
-        //         temp.next = a;
-        //         temp = a;
-        //         temp2 = temp2.next;
-        //     }
-        // }
-        // if(temp1==null){
-        //     temp.next = temp2;
-        // }else{
-        //     temp.next = temp1;
-        // }
-        // return head.next;
-
-        ListNode head = new ListNode(3423);
-        ListNode temp = head;
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        while(temp1!=null && temp2!=null){
-            if(temp1.val<temp2.val){
-                ListNode a = new ListNode(temp1.val);
-                temp.next = a;
-                temp = a;
-                temp1 = temp1.next;
-            }else{
-                ListNode a = new ListNode(temp2.val);
-                temp.next = a;
-                temp = a;
-                temp2 = temp2.next;
+        if(list1==null&&list2==null)return null;
+        if(list1==null&&list2!=null)return list2;
+        if(list1!=null&&list2==null)return list1;
+        ListNode head = new ListNode(10000);
+        ListNode temp=head;
+        ListNode t1 = list1;
+        ListNode t2 = list2;
+        while(t1!=null && t2!=null){
+            if(t1.val<t2.val){
+                ListNode Tsum = new ListNode(t1.val);
+                temp.next = Tsum;
+                temp=temp.next;
+                t1 = t1.next;
+            }
+            else{
+                ListNode Tsum = new ListNode(t2.val);
+                temp.next = Tsum;
+                temp=temp.next;
+                t2 = t2.next;
             }
         }
-        if(temp1==null){
-            temp.next = temp2;
-        }else{
-            temp.next = temp1;
+        
+        if(t1==null){
+            temp.next=t2;
+        }
+        if(t2==null){
+            temp.next=t1;
         }
         return head.next;
     }
